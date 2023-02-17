0359ec9e5a4370666425f7d01843a50118415df2
mateuszezil <mwasilewski92@gmail.com>
2023-02-17 16:07:43 +0100

    Zrobione

---
+++ halls_app/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import Hall, ReservationHall
+
+# Register your models here.
+admin.site.register(Hall)
+admin.site.register(ReservationHall)
--- halls_app/urls.py
+++ halls_app/urls.py
@@ -3,7 +3,11 @@ from halls_app import views
 
 urlpatterns = [
     path('new/', views.add_hall),
-    path('<int:id>/', views.show_all),
-    path('delete/<int:id>/', views.hall_remove)
+    path('<int:hall_id>/', views.show_hall),
+    path('delete/<int:hall_id>/', views.hall_remove),
+    path('redirect/', views.show_hall),
+    path('home/', views.show_all),
+    path('modify/<int:hall_id>/', views.modify_hall),
+    path('reserve/<int:hall_id>/', views.reserve_hall),
 
 ]
